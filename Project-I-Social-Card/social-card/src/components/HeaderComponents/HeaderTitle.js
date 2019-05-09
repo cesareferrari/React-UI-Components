b7c5255d38714c992ff6bbbfd65a5ad9b3282cec
Cesare Ferrari <cesare@maryandferrari.com>
2019-05-08 21:55:04 -0400

    Create header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div>
+    <h3>Lambda School</h3>
+    <span>@LambdaSchool - 26 jan</span>
+    </div>
+  )
+}
+
+export default HeaderTitle;
