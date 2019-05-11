52ad4bb2471ad253f4121b1343bfeb45197f752f
Cesare Ferrari <cesare@maryandferrari.com>
2019-05-11 12:55:10 -0400

    Style action buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
     <div>
-      <NumberButton buttonStyle="number-button" text="0" />
-      <NumberButton buttonStyle="number-button"  text="1" />
+      <NumberButton buttonStyle="number-button" text="1" />
+      <NumberButton buttonStyle="number-button"  text="2" />
+      <ActionButton buttonStyle="number-button action-button"  text="0" />
+      <ActionButton buttonStyle="number-button action-button normal"  text="clear" />
     </div>
   );
 };
