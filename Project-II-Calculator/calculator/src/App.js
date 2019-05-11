6eb6054ea7563dbf7ddc7ecb4eb592767611675e
Cesare Ferrari <cesare@maryandferrari.com>
2019-05-11 12:48:39 -0400

    Style number buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -5,8 +5,8 @@ import NumberButton from './components/ButtonComponents/NumberButton';
 const App = () => {
   return (
     <div>
-      <NumberButton text="0" />
-      <NumberButton text="1" />
+      <NumberButton buttonStyle="number-button" text="0" />
+      <NumberButton buttonStyle="number-button"  text="1" />
     </div>
   );
 };
