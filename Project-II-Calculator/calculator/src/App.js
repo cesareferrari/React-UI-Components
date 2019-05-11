0aac1a94e76fc06d8a867e37b6d8634586601e42
Cesare Ferrari <cesare@maryandferrari.com>
2019-05-11 13:41:17 -0400

    Add symbol buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
 
-
-
 const App = () => {
 
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9].reverse();
@@ -13,10 +11,23 @@ const App = () => {
     return <NumberButton key={number} buttonStyle="number-button" text={number} />
   })
 
+  const division = '\u00F7'
+  const multiplication = '\u00D7'
+  const addition = '\u002B'
+  const subtraction = '\u2212'
+  const equals = '\u003D'
+
+  const symbols = [division, multiplication, addition, subtraction, equals];
+
+  const symbolButtons = symbols.map(symbol => {
+    return <NumberButton key={symbol} buttonStyle="number-button symbol" text={symbol} />
+  })
+
   return (
     <div>
       <ActionButton buttonStyle="number-button action-button normal"  text="clear" />
       {numberButtons}
+      {symbolButtons}
       <ActionButton buttonStyle="number-button action-button"  text="0" />
     </div>
   );
