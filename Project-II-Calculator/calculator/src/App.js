b96d5bf3e840dfffeb82c747dbe10c5fd7207e3b
Cesare Ferrari <cesare@maryandferrari.com>
2019-05-11 13:13:20 -0400

    Add number buttons from array

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
 
+
+
 const App = () => {
+
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9].reverse();
+
+  const numberButtons = numbers.map(number => {
+    return <NumberButton key={number} buttonStyle="number-button" text={number} />
+  })
+
   return (
     <div>
-      <NumberButton buttonStyle="number-button" text="1" />
-      <NumberButton buttonStyle="number-button"  text="2" />
-      <ActionButton buttonStyle="number-button action-button"  text="0" />
       <ActionButton buttonStyle="number-button action-button normal"  text="clear" />
+      {numberButtons}
+      <ActionButton buttonStyle="number-button action-button"  text="0" />
     </div>
   );
 };
